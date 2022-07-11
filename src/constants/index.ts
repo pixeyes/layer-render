884e24f16a9b9879c5ca7d5fea8791f4b4b4b58e
zhhyang <zhhyang0404@163.com>
2022-07-11 18:01:25 +0800

    fix:切换视觉稿时自动调整距离顶部的距离

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+/**
+ * 默认距离顶部的距离
+ */
+export const DEFAULT_TOP = -110;
--- src/render/index.tsx
+++ src/render/index.tsx
@@ -22,6 +22,7 @@ import Context, { IContext } from "../context";
 import { artSizeList } from "../constants/unit";
 import { Spin } from "antd";
 import { base64Encode } from "../utils/imgUtil";
+import { DEFAULT_TOP } from "../constants";
 
 export interface LayerRenderProps extends Partial<ControlTypeProps> {
   data: any;
@@ -254,7 +255,7 @@ class LayerRender extends React.Component<LayerRenderProps, State> {
         bottom: null,
         left: null,
         x: 0,
-        y: 0,
+        y: DEFAULT_TOP,
         data: this.props.data,
       });
       this.loadImg();
